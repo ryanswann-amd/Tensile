af905d4c689e2085252a2534d1b35c52dbe1d64d
Ryan Swann <ryan.swann@amd.com>
2024-04-02 15:21:52 -0700

    Remove extra flag

--- Tensile/KernelWriterAssembly.py
+++ Tensile/KernelWriterAssembly.py
@@ -12954,7 +12954,7 @@ class KernelWriterAssembly(KernelWriter):
       kStr += inst("v_mov_b32 ", vgpr(flagReg), 1, "flag data")
       kStr += inst("v_mov_b32 ", vgpr(flagOffsetReg), sgpr(tmpSgpr), "flag offset into vgpr")
       kStr += inst("s_mov_b64  ", "exec", "0x0000000000000001", "set exec mask for only the first thread")
-      kStr += inst("global_store_dword ", vgpr(flagOffsetReg), vgpr(flagReg), sgpr("AddressFlags", 2), "sc0", "set flag")
+      kStr += inst("global_store_dword ", vgpr(flagOffsetReg), vgpr(flagReg), sgpr("AddressFlags", 2), "set flag")
       kStr += inst("s_mov_b64  ", "exec", "0xFFFFFFFFFFFFFFFF", "reset exec mask for all threads")
       kStr += inst("s_waitcnt", "lgkmcnt(0)", "vmcnt(0)", "wait for flag") # TODO just for testing
     else:
@@ -13577,7 +13577,7 @@ class KernelWriterAssembly(KernelWriter):
       if self.version == (11,0,0) or self.version == (9,4,2): # TODO: Temporary workaround
         flagOffsetReg = tmpVgpr
         kStr += inst("v_mov_b32 ", vgpr(flagOffsetReg), sgpr(tmpSgpr), "flag offset into vgpr")
-        kStr += inst("global_load_dword ", vgpr(flagOffsetReg), vgpr(flagOffsetReg), sgpr("AddressFlags", 2), "sc0", "get flag")
+        kStr += inst("global_load_dword ", vgpr(flagOffsetReg), vgpr(flagOffsetReg), sgpr("AddressFlags", 2), "get flag")
         kStr += inst("s_waitcnt", "lgkmcnt(0)", "wait for flag load")
         kStr += inst("v_readlane_b32", sgpr(tmpSgpr+2), vgpr(flagOffsetReg), "0x0000000000000001", "flag to sgpr")
       else:
